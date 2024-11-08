1098445cb3018bdd33bcbce3976ea44f336b73c4
dcabriales <69397120+dcabriales@users.noreply.github.com>
2024-11-08 14:30:11 -0600

    reinput data for website to wrk

--- website/__init__.py
+++ website/__init__.py
@@ -21,6 +21,12 @@ def create_app():
     with app.app_context():
         # oldd = Cycle(start_date=datetime.date(2024,5,31),end_date=datetime.date(2024,6,13),user=1)
         # new = Cycle(start_date=datetime.date(2024,6,14),end_date=datetime.date(2024,6,27),user=1)
+        
+        # create_user = User("Cabriales", datetime.date(2024,8, 15))
+        # db.session.delete(create_user)
+        # new_inc = Income(300, datetime.date(2024, 11, 1),1)
+        # db.session.add(new_inc)
+        db.session.commit()
         db.create_all()
 
     return app
