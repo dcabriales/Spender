9db15b9f75c8166df268144d73fc84c60960e27a
dcabriales <69397120+dcabriales@users.noreply.github.com>
2024-11-12 13:34:12 -0600

    uncommented session.commit

--- website/__init__.py
+++ website/__init__.py
@@ -28,7 +28,7 @@ def create_app():
         # # db.session.delete(create_user)
         # new_inc = Income(300, datetime.date(2024, 11, 1),1)
         # db.session.add(new_inc)
-        # db.session.commit()
+        db.session.commit()
         db.create_all()
 
     return app
